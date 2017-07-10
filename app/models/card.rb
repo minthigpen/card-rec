00b5925f6222bf418f5e253f5b037329725fb8bd
Minsoo <mthigpen@paperlesspost.com>
2017-07-10 11:03:19 -0400

    created subclasses Card and Background and finished importing all image URLS

---
+++ app/models/card.rb
@@ -0,0 +1,4 @@
+class Card < Image
+
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170707214016) do
+ActiveRecord::Schema.define(version: 20170707221326) do
 
   create_table "colors", force: :cascade do |t|
     t.integer "image_id"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20170707214016) do
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "type"
   end
 
 end
