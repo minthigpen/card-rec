2e934276d7de02044433e407317ff8fa85dcd264
Rob Esris <resris@paperlesspost.com>
2017-07-27 13:19:33 -0400

    Normalize color swatches using presenters

--- app/presenters/match_presenter.rb
+++ app/presenters/match_presenter.rb
@@ -1,7 +1,11 @@
 class MatchPresenter
+  attr_reader :match, :swatch_presenter
+
+  delegate :id, :card, :background, :rule, :score, :best_score, to: :match
+  delegate :normalized_colors, to: :swatch_presenter
 
   def initialize(match)
-    @match = options[:match]
+    @match = match
     @swatch_presenter = SwatchPresenter.new(@match.background)
   end
 
--- app/presenters/swatch_presenter.rb
+++ app/presenters/swatch_presenter.rb
@@ -4,11 +4,12 @@ class SwatchPresenter
   def initialize(image)
     colors = image.colors.sort{ |color_1, color_2| color_2.pixel_fraction <=> color_1.pixel_fraction }
     
-    fraction_sum = @colors.sum(&:pixel_fraction)
+    fraction_sum = colors.sum(&:pixel_fraction)
     multiplier = 1.0 / fraction_sum
     @normalized_colors = colors.map do |color|
       color_dup = color.dup
       color_dup.pixel_fraction = color.pixel_fraction * multiplier
+      color_dup
     end
   end
 
