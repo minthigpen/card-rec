ad0a4eede05455345c1f039538ae31c0bc6b5933
Minsoo <mthigpen@paperlesspost.com>
2017-07-27 16:58:08 -0400

    stylesheet merge

--- app/models/rule.rb
+++ app/models/rule.rb
@@ -4,19 +4,6 @@ class Rule < ApplicationRecord
   PIX_FRAC_THRESHOLD = 0.001
   SCORE_THRESHOLD = 0.01
 
-  # helper method to get color difference of two RGB objects
-  def self.get_color_diff (rgb_color1, rgb_color2)
-    # make dummy rgb object
-    @dummy_rgb ||= Color::RGB.new(0,0,0)
-    # convert RGB objects to CIE Lab Objects
-    lab_color1 = rgb_color1.to_lab
-    lab_color2 = rgb_color2.to_lab
-    # return difference
-    @dummy_rgb.delta_e94(lab_color1, lab_color2)
-
-  end
-
-
   # get a card's color profile and find a backdrop that is the most closely associated to 
   # the complementary color of the card's 'main color'
   def self.compl_color(card, background)
@@ -64,35 +51,38 @@ class Rule < ApplicationRecord
     card_colors = card.colors.sort{|a, b| a.pixel_fraction <=> b.pixel_fraction}.reverse!
     background_colors = background.colors.sort {|a, b| a.score <=> b.score}.reverse!
     # convert the sorted array to an array of RGB objects
-    card_rgb = to_rgb_obj(card_colors)
-    background_rgb = to_rgb_obj(background_colors)
+    # card_rgb = to_rgb_obj(card_colors.last)
+    background_rgb = to_rgb_obj([background_colors.first])
     # set the card highlight color as card color with the second most pixel fraction if the array contains more than one color
-    card_rgb.size > 1 ? card_highlight_rgb = card_rgb[1][0] : card_highlight_rgb = card_rgb[0][0]
+    # card_rgb.size > 1 ? card_highlight_rgb = card_rgb[1][0] : card_highlight_rgb = card_rgb[0][0]
+    card_highlight_rgb = to_rgb_obj([card_colors.last])
     # compare the card highlight color to the main background rgb color with highest score
-    get_color_diff(card_highlight_rgb, background_rgb[0][0])
+    get_color_diff(card_highlight_rgb[0][0], background_rgb[0][0])
 
   end
 
   # Get a card's color profile and find a backdrop that is the most closely associated to 
   # the analogous colors of the card's 'main color'
   def self.analogous_color(card, background)
-     # get the card and background color with the highest score and convert to hsv
-    # best_card_color = get_best_color_hsv(card.colors)
-    # best_background_color = get_best_color_hsv(background.colors)
+    # sort card colors by score to get the 'main focus' color of the card since a lot of cards are on white backgrounds
+    card_colors = card.colors.sort{|a, b| a.score <=> b.score}.reverse!
+    # sort background colors by pixel fraction since backgrounds are simpler
+    background_colors = background.colors.sort {|a, b| a.pixel_fraction <=> b.pixel_fraction}.reverse!
+    background_rgb = to_rgb_obj(background_colors)
+    # convert to HSL in order to get the two analogous colors
+    analog_card_color1 = Color::RGB.new(card_colors.first.red.to_i, card_colors.first.green.to_i, card_colors.first.blue.to_i).to_hsl
+    analog_card_color2 = Color::RGB.new(card_colors.first.red.to_i, card_colors.first.green.to_i, card_colors.first.blue.to_i).to_hsl
+    analog_card_color1.hue=(analog_card_color1.hue+30)
+    analog_card_color2.hue=(analog_card_color2.hue-30)
 
-    # # get the hue of the two analogous colors 
-    # analogous_hues = [(best_card_color.first + 30) % 360,(best_card_color.first - 30).abs % 360]
-
-
-    # acolor1 = [analogous_hues[0], best_card_color[1],best_card_color[2]]
-    # acolor2 = [analogous_hues[1], best_card_color[1],best_card_color[2]]
-
-    # # get difference of the two analogous colors and get the lowest score
-    # cd1 = get_color_diff(acolor1,best_background_color)
-    # cd2 = get_color_diff(acolor2,best_background_color)
-
-    # [cd1,cd2].min
-    rand
+    # convert back to array of RGB objects
+    analog_card_colors = [analog_card_color1.to_rgb, analog_card_color2.to_rgb]
+    # get difference of the two analogous colors with the best background color and get the lowest score
+    diff = []
+    analog_card_colors.each do |a_color|
+      diff << get_color_diff(a_color, background_rgb[0][0])
+    end
+    diff.min
   end
 
   def self.contrast(card, background)
@@ -116,6 +106,17 @@ class Rule < ApplicationRecord
     color_profile.map do |color|
       [Color::RGB.new(color.red.to_i, color.green.to_i, color.blue.to_i), color.score, color.pixel_fraction]
     end
+  end
+
+  # helper method to get color difference of two RGB objects
+  def self.get_color_diff (rgb_color1, rgb_color2)
+    # make dummy rgb object
+    @dummy_rgb ||= Color::RGB.new(0,0,0)
+    # convert RGB objects to CIE Lab Objects
+    lab_color1 = rgb_color1.to_lab
+    lab_color2 = rgb_color2.to_lab
+    # return difference
+    @dummy_rgb.delta_e94(lab_color1, lab_color2)
   end
 
 end
