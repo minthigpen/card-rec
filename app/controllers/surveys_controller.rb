0337fa0c17f05f5731114c93eda599ee131d2210
Rob Esris <resris@paperlesspost.com>
2017-07-27 17:24:16 -0400

    Optimize matching to only store best matches

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -18,7 +18,7 @@ class SurveysController < ApplicationController
     # get card and make matches and store those in instance variable 
     @card = params[:card_id] ? Card.find(params[:card_id]) : Card.order("RANDOM()").first
     @card_swatch_colors = SwatchPresenter.new(@card).normalized_colors
-    @card.initialize_matches_and_mark_best_scores unless @card.matches.any? 
+    @card.initialize_matches unless @card.matches.any?
     @matches = @card.best_matches.order(:rule_id).includes(background: :colors).map{ |match| MatchPresenter.new(match) }
 
     @survey = Survey.new
--- app/models/card.rb
+++ app/models/card.rb
@@ -3,11 +3,6 @@ class Card < Image
   has_many :matches
   has_many :backgrounds, through: :matches
 
-  def initialize_matches_and_mark_best_scores
-    initialize_matches
-    mark_best_scores
-  end
-
   def best_matches
     matches.where(best_score: true)
   end
@@ -16,26 +11,20 @@ class Card < Image
   private
 
   def initialize_matches 
-    # loop through all the background
-    Background.all.each do |b|
-      # create match for each rule
-      Rule.all.each do |r|
-        
-        score = Rule.send(r.name, self, b)
-        # create new match obj
-        self.matches << Match.create(background: b, rule: r, score: score, best_score: false)
+    Rule.all.each do |rule|
+      best_score = Float::INFINITY
+      best_match_params = {}
 
+      Background.includes(:colors).all.each do |background|
+        score = Rule.send(rule.name, self, background)
+        if score < best_score
+          best_score = score
+          best_match_params = {background: background, rule: rule, score: score, best_score: true}
+        end
       end
-    end
-  end
 
-  def mark_best_scores
-    Rule.all.each do |r|
-      # get the score that's the lowest because that's the background image with the smallest difference from target color space
-      best_match = self.matches.where(rule: r).order(score: :desc).last
-      puts "#{Background.find(best_match.background_id).colors.first}"
-      best_match.best_score = true
-      best_match.save!
+      self.matches << Match.create(best_match_params)
+
     end
   end
 
