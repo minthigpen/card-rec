a5388b239404faf1ac20ee4529428ff267750613
Minsoo <mthigpen@paperlesspost.com>
2017-07-27 12:33:29 -0400

    re-did the highlight and similarity rules using Color ruby gem

--- app/models/rule.rb
+++ app/models/rule.rb
@@ -4,100 +4,103 @@ class Rule < ApplicationRecord
   PIX_FRAC_THRESHOLD = 0.001
   SCORE_THRESHOLD = 0.01
 
+
+  def self.get_color_diff (rgb_color1, rgb_color2)
+    # make dummy rgb object
+    @dummy_rgb ||= Color::RGB.new(0,0,0)
+    # convert RGB objects to CIE Lab Objects
+    lab_color1 = rgb_color1.to_lab
+    lab_color2 = rgb_color2.to_lab
+    # return difference
+    @dummy_rgb.delta_e94(lab_color1, lab_color2)
+
+  end
+
+
   # get a card's color profile and find a backdrop that is the most closely associated to 
   # the complementary color of the card's 'main color'
   def self.compl_color(card, background)
+
+    # if gray scale, just show the opposite end of scale
 
     # there are several different colors that you could find the complement of - either the most highest score color or highest pixel fraction color 
       # though edge case is that often the most dominant colors are the white background of a card. make this a method so that you can reuse for analagous
 
     # get the most dominant colors for cards and backgrounds
-    card_colors = card.colors.where('pixel_fraction > ?', PIX_FRAC_THRESHOLD).where('score > ?', SCORE_THRESHOLD)
-    background_colors = background.colors.where('pixel_fraction > ?', PIX_FRAC_THRESHOLD).where('score > ?', SCORE_THRESHOLD)
+    card_colors = card.colors.where('pixel_fraction > ?', PIX_FRAC_THRESHOLD).where('score > ?', SCORE_THRESHOLD).to_a
+    background_colors = background.colors.where('pixel_fraction > ?', PIX_FRAC_THRESHOLD).where('score > ?', SCORE_THRESHOLD).to_a
 
-    # get the card and background color with the highest score and convert to hsv
-    best_card_color = get_best_color_hsv(card.colors)
-    best_background_color = get_best_color_hsv(background.colors)
+    rand
+    # # get the card and background color with the highest score and convert to hsv
+    # best_card_color = get_best_color_hsv(card.colors)
+    # best_background_color = get_best_color_hsv(background.colors)
 
-    # get the hue of the complimentary color 
-    comp_hue = (best_card_color.first + 180) % 360
-    # also get the comp_value and comp_saturation ***********
+    # # get the hue of the complimentary color 
+    # comp_hue = (best_card_color.first + 180) % 360
+    # # also get the comp_value and comp_saturation ***********
 
-    # if card color is lighter than
-    card_color = [comp_hue, best_card_color[1],best_card_color[2]]
-    puts "HSV best card color is #{best_card_color}"
-    puts "HSV best background color is #{best_background_color}"
-    puts "HSV for COMPLEMENTARY COLOR IS #{card_color}"
+    # # if card color is lighter than
+    # card_color = [comp_hue, best_card_color[1],best_card_color[2]]
+    # puts "HSV best card color is #{best_card_color}"
+    # puts "HSV best background color is #{best_background_color}"
+    # puts "HSV for COMPLEMENTARY COLOR IS #{card_color}"
 
-    # get difference of colors and that's the score
-    cd = get_color_diff(card_color,best_background_color)
+    # # get difference of colors and that's the score
+    # cd = get_color_diff(card_color,best_background_color)
 
 
 
   end
 
   def self.similarity(card, background)
-    # need to take into account similar color profile instead of just single color swatch that is the most dominant
-    
-    # take into account both score and pixel fraction
-    # get the most dominant colors for cards and backgrounds
-    # card_colors = card.colors.where('pixel_fraction > ?', PIX_FRAC_THRESHOLD).where('score > ?', SCORE_THRESHOLD).order(score: :desc).limit(3)
-    # background_colors = background.colors.where('pixel_fraction > ?', PIX_FRAC_THRESHOLD).where('score > ?', SCORE_THRESHOLD).order(score: :desc).limit(3)
-    diff = []
+    # sort colors related to card and background by score
+    card_colors = card.colors.sort{|a, b| a.score <=> b.score}
+    background_colors = background.colors.sort {|a, b| a.score <=> b.score}
 
-    # sort by score
-    c = card.colors.sort{|a, b| a.score <=> b.score}
-    b = background.colors.sort {|a, b| a.score <=> b.score}
-    # sort by pixel fraction
-    card_colors_sorted = c.sort_by{ |a| -a.pixel_fraction}
+    # turn into rgb objects
+    card_rgb = to_rgb_obj(card_colors)
+    background_rgb = to_rgb_obj(background_colors)
 
-    d = []
-    0.upto([c.size, b.size].min - 1) do |index|
-      d << get_color_diff(c[index].to_hsv, b[index].to_hsv)
+    # get the color differences of the most dominant colors and average them 
+    diff_colors_array = []
+
+    0.upto([card_rgb.size, background_rgb.size].min - 1) do |index|
+      diff_colors_array << get_color_diff(card_rgb[index][0], background_rgb[index][0])
     end
 
-
-    # c = Array.new
-    # b = Array.new
-    # # compare the first three ordered by pixel_fraction
-    # card_colors.order(pixel_fraction: :desc).each do |card|
-    #   c.push(card.to_hsv)
-    # end
-
-    # background_colors.order(pixel_fraction: :desc).each do |back|
-    #   b.push(back.to_hsv)
-    # end
-
-    d.inject(:+).to_f / d.size
+    diff_colors_array.inject(:+).to_f / diff_colors_array.size
   end
 
   def self.highlight(card, background)
-    # highlights just take the color that is the most dominant (regardless of pixel fraction and picks a background that highlights this color
-    best_card_color = get_best_color_hsv(card.colors)
-    best_background_color = get_best_color_hsv(background.colors)
+    # # highlights just take the color that is the most dominant (regardless of pixel fraction and picks a background that highlights this color
+    card_highlight_color = card.colors.max_by(&:score)
+    background_highlight_color = background.colors.max_by(&:score)
 
-    # maybe consider lower saturation?
-    get_color_diff(best_card_color, best_background_color)
+    card_highlight_rgb = Color::RGB.new(card_highlight_color.red.to_i, card_highlight_color.green.to_i, card_highlight_color.blue.to_i)
+    background_highlight_rgb = Color::RGB.new(background_highlight_color.red.to_i, background_highlight_color.green.to_i, background_highlight_color.blue.to_i)
+
+    get_color_diff(card_highlight_rgb, background_highlight_rgb)
 
   end
 
   def self.analogous_color(card, background)
      # get the card and background color with the highest score and convert to hsv
-    best_card_color = get_best_color_hsv(card.colors)
-    best_background_color = get_best_color_hsv(background.colors)
+    # best_card_color = get_best_color_hsv(card.colors)
+    # best_background_color = get_best_color_hsv(background.colors)
 
-    # get the hue of the two analogous colors 
-    analogous_hues = [(best_card_color.first + 30) % 360,(best_card_color.first - 30).abs % 360]
+    # # get the hue of the two analogous colors 
+    # analogous_hues = [(best_card_color.first + 30) % 360,(best_card_color.first - 30).abs % 360]
 
 
-    acolor1 = [analogous_hues[0], best_card_color[1],best_card_color[2]]
-    acolor2 = [analogous_hues[1], best_card_color[1],best_card_color[2]]
+    # acolor1 = [analogous_hues[0], best_card_color[1],best_card_color[2]]
+    # acolor2 = [analogous_hues[1], best_card_color[1],best_card_color[2]]
 
-    # get difference of the two analogous colors and get the lowest score
-    cd1 = get_color_diff(acolor1,best_background_color)
-    cd2 = get_color_diff(acolor2,best_background_color)
+    # # get difference of the two analogous colors and get the lowest score
+    # cd1 = get_color_diff(acolor1,best_background_color)
+    # cd2 = get_color_diff(acolor2,best_background_color)
 
-    [cd1,cd2].min
+    # [cd1,cd2].min
+    rand
   end
 
   def self.contrast(card, background)
@@ -116,37 +119,11 @@ class Rule < ApplicationRecord
     rand
   end
 
-  # input colors
-  def self.get_best_color_hsv (colors)
-    # get the color with max score
-    best_color = colors.max_by {|x| x.score}
-    # convert to hsv
-    best_color.to_hsv
+  # helper method to return array of RGB objects
+  def self.to_rgb_obj (color_profile)
+    color_profile.map do |color|
+      [Color::RGB.new(color.red.to_i, color.green.to_i, color.blue.to_i), color.score, color.pixel_fraction]
+    end
   end
-
-    # input two colors from the CIE Lab space
-  def self.get_color_diff(hue1, hue2)
-    # convert from HSV to RGB to XYZ to CIEL*ab then take the DELTA E using ::Color Module
-
-
-
-
-
-    # CARTESIAN DISTANCE BETWEEN TWO HSV TUPLES
-
-    h0,s0,v0 = hue1[0],hue1[1], hue1[2]
-    h1,s1,v1 = hue2[0],hue2[1], hue2[2]
-    # distance between two hues
-    dh = [(h1-h0).abs,(360-(h1-h0).abs)].min/180.0
-
-    ds = (s1-s0).abs
-    dv = (v1-v0).abs
-
-    # Each of these values will be in the range [0,1]. You can compute the length of this tuple:
-
-    distance = Math.sqrt(dh*dh+ds*ds+dv*dv)
-  end
-
-
 
 end
