813743610075067073b6f0c827ba69064211f93b
Rob Esris <resris@paperlesspost.com>
2017-07-31 15:35:40 -0400

    init matches

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,6 +2,7 @@ class CardsController < ApplicationController
 
   def recommended_backgrounds
     card = Card.find_by_pp_id(params[:new_paper_id])
+    card.initialize_matches
     recommended_background_ids = card.best_matches.map do |match|
       match.background.pp_id
     end
