3df903465fd6fba4447c209eab4a3b478cde22d4
Minsoo <mthigpen@paperlesspost.com>
2017-08-01 16:19:13 -0400

    fixes to compl_color and analogous_color rules

--- app/models/rule.rb
+++ app/models/rule.rb
@@ -18,17 +18,24 @@ class Rule < ApplicationRecord
     # without the white color, assuming the list of card colors are ranked by score
     main_card_color = card_colors_hsl.first
 
-    # change the hue
+    # change the hue, saturation and luminosity of main card
     main_card_color.hue=(main_card_color.hue-180)
-    # main_card_color.saturation=(0)
-    # main_card_color.luminosity=(1)
+    main_card_color.luminosity=(50)
+    main_card_color.saturation=(100)
+
+    puts "COMPLEMENTARY COLOR is #{main_card_color.hue}, #{main_card_color.saturation}, #{main_card_color.luminosity} "
     compl_card_color = main_card_color
 
-    # get the difference of the target complementary color for the main card color and the background color with the highest pixel fraction
+    # convert the saturation/lightness of the background in order to get a consistent difference using delta E
     best_background_color = Color::RGB.new(background_colors.first.red.to_i, background_colors.first.green.to_i, background_colors.first.blue.to_i)
-    get_color_diff(compl_card_color.to_rgb, best_background_color)
+    background_hsl = best_background_color.to_hsl
+    background_hsl.luminosity=(50)
+    background_hsl.saturation=(100)
+    background_rgb = background_hsl.to_rgb
 
-    # # also get the comp_value and comp_saturation *********** --> this might be implemented in contrast
+    # get the difference of the target complementary color for the main card color and the background color with the highest pixel fraction
+    get_color_diff(compl_card_color.to_rgb, background_rgb)
+
 
   end
 
@@ -83,15 +90,26 @@ class Rule < ApplicationRecord
     # convert to HSL in order to get the two analogous colors
     analog_card_color1 = Color::RGB.new(card_colors.first.red.to_i, card_colors.first.green.to_i, card_colors.first.blue.to_i).to_hsl
     analog_card_color2 = Color::RGB.new(card_colors.first.red.to_i, card_colors.first.green.to_i, card_colors.first.blue.to_i).to_hsl
+    # set HSL for analogous color 1
     analog_card_color1.hue=(analog_card_color1.hue+30)
+    analog_card_color1.luminosity=(50)
+    analog_card_color1.saturation=(100)
+    # set HSL for analogous color 2
     analog_card_color2.hue=(analog_card_color2.hue-30)
+    analog_card_color2.luminosity=(50)
+    analog_card_color2.saturation=(100)
+
+    background_hsl = background_rgb[0][0].to_hsl
+    background_hsl.luminosity=(50)
+    background_hsl.saturation=(100)
+    background_rgb = background_hsl.to_rgb
 
     # convert back to array of RGB objects
     analog_card_colors = [analog_card_color1.to_rgb, analog_card_color2.to_rgb]
     # get difference of the two analogous colors with the best background color and get the lowest score
     diff = []
     analog_card_colors.each do |a_color|
-      diff << get_color_diff(a_color, background_rgb[0][0])
+      diff << get_color_diff(a_color, background_rgb)
     end
     diff.min
 
@@ -149,8 +167,43 @@ class Rule < ApplicationRecord
     # convert RGB objects to CIE Lab Objects
     lab_color1 = rgb_color1.to_lab
     lab_color2 = rgb_color2.to_lab
-    # return difference
+    # return difference with Ruby gem for Color
     @dummy_rgb.delta_e94(lab_color1, lab_color2)
+    # puts (@dummy_rgb.delta_e94(lab_color1, lab_color2))
+    # return the difference with just manual computation
+
+    # k_1 = 0.045
+    # k_2 = 0.015
+    # k_L = 1
+
+    # k_C = k_H = 1
+
+    # l_1, a_1, b_1 = lab_color1.values_at(:L, :a, :b)
+    # puts ("#{l_1},#{a_1},#{b_1}")
+    # l_2, a_2, b_2 = lab_color2.values_at(:L, :a, :b)
+
+    # delta_a = a_1 - a_2
+    # delta_b = b_1 - b_2
+
+    # c_1 = Math.sqrt((a_1 ** 2) + (b_1 ** 2))
+    # c_2 = Math.sqrt((a_2 ** 2) + (b_2 ** 2))
+
+    # delta_L = lab_color1[:L] - lab_color2[:L]
+    # delta_C = c_1 - c_2
+
+    # delta_H2 = (delta_a ** 2) + (delta_b ** 2) - (delta_C ** 2)
+
+    # s_L = 1
+    # s_C = 1 + k_1 * c_1
+    # s_H = 1 + k_2 * c_1
+
+    # composite_L = (delta_L / (k_L * s_L)) ** 2
+    # composite_C = (delta_C / (k_C * s_C)) ** 2
+    # composite_H = delta_H2 / ((k_H * s_H) ** 2)
+    # puts (Math.sqrt(composite_L + composite_C + composite_H))
+    # Math.sqrt(composite_L + composite_C + composite_H)
+
+
   end
 
 end
