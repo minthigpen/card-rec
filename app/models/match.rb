365e9cbe86630f3572a3ca78bdd3da39e5e4f433
Minsoo <mthigpen@paperlesspost.com>
2017-08-03 18:58:18 -0400

    def function for returning Beta sample

--- app/models/match.rb
+++ app/models/match.rb
@@ -4,7 +4,7 @@ class Match < ApplicationRecord
   belongs_to :background
   belongs_to :rule
 
-  def beta_samp(a,b)
+  def beta_sample(a,b)
     # return sampling score
     SimpleRandom.new.beta(a, b)
   end
